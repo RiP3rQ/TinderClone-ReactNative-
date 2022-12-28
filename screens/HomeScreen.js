05043d5a20c089fbd8abed666242f46300710581
RiP3rQ <rafalpompa2000@gmail.com>
2022-12-28 20:04:03 +0100

    adding firebase to project

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,5 +1,4 @@
 import { View, Text, Button } from "react-native";
-import React from "react";
 import { useNavigation } from "@react-navigation/native";
 
 const HomeScreen = () => {
--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,5 +1,4 @@
 import { View, Text } from "react-native";
-import React from "react";
 import useAuth from "../hooks/useAuth";
 
 const LoginScreen = () => {
