8b5ed4451128a0d063948cee3a23de359c87b963
RiP3rQ <rafalpompa2000@gmail.com>
2022-12-28 19:54:34 +0100

    fixing stack navigator

--- App.js
+++ App.js
@@ -1,5 +1,14 @@
+import { NavigationContainer } from "@react-navigation/native";
+import { AuthProvider } from "./hooks/useAuth";
 import StackNavigator from "./StackNavigator";
 
 export default function App() {
-  return <StackNavigator />;
+  return (
+    <NavigationContainer>
+      {/* HOC - HIGHER ORDER COMPONENT */}
+      <AuthProvider>
+        <StackNavigator />
+      </AuthProvider>
+    </NavigationContainer>
+  );
 }
--- StackNavigator.js
+++ StackNavigator.js
@@ -1,31 +1,25 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { NavigationContainer } from "@react-navigation/native";
 import HomeScreen from "./screens/HomeScreen";
 import ChatScreen from "./screens/ChatScreen";
 import LoginScreen from "./screens/LoginScreen";
-import { AuthProvider } from "./hooks/useAuth";
+import useAuth, { AuthProvider } from "./hooks/useAuth";
 
 const Stack = createNativeStackNavigator();
 
 const StackNavigator = () => {
-  const user = false;
+  const { user } = useAuth();
 
   return (
-    <NavigationContainer>
-      {/* HOC - HIGHER ORDER COMPONENT */}
-      <AuthProvider>
-        <Stack.Navigator>
-          {user ? (
-            <>
-              <Stack.Screen name="Home" component={HomeScreen} />
-              <Stack.Screen name="Chat" component={ChatScreen} />
-            </>
-          ) : (
-            <Stack.Screen name="Login" component={LoginScreen} />
-          )}
-        </Stack.Navigator>
-      </AuthProvider>
-    </NavigationContainer>
+    <Stack.Navigator>
+      {user ? (
+        <>
+          <Stack.Screen name="Home" component={HomeScreen} />
+          <Stack.Screen name="Chat" component={ChatScreen} />
+        </>
+      ) : (
+        <Stack.Screen name="Login" component={LoginScreen} />
+      )}
+    </Stack.Navigator>
   );
 };
 
