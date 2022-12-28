e3106805cc9a13f9313b3f8f25a85b11eb55966f
RiP3rQ <rafalpompa2000@gmail.com>
2022-12-28 19:36:41 +0100

    implementing context API + custom hook useAuth

--- StackNavigator.js
+++ StackNavigator.js
@@ -3,6 +3,7 @@ import { NavigationContainer } from "@react-navigation/native";
 import HomeScreen from "./screens/HomeScreen";
 import ChatScreen from "./screens/ChatScreen";
 import LoginScreen from "./screens/LoginScreen";
+import { AuthProvider } from "./hooks/useAuth";
 
 const Stack = createNativeStackNavigator();
 
@@ -11,16 +12,19 @@ const StackNavigator = () => {
 
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        {user ? (
-          <>
-            <Stack.Screen name="Home" component={HomeScreen} />
-            <Stack.Screen name="Chat" component={ChatScreen} />
-          </>
-        ) : (
-          <Stack.Screen name="Login" component={LoginScreen} />
-        )}
-      </Stack.Navigator>
+      {/* HOC - HIGHER ORDER COMPONENT */}
+      <AuthProvider>
+        <Stack.Navigator>
+          {user ? (
+            <>
+              <Stack.Screen name="Home" component={HomeScreen} />
+              <Stack.Screen name="Chat" component={ChatScreen} />
+            </>
+          ) : (
+            <Stack.Screen name="Login" component={LoginScreen} />
+          )}
+        </Stack.Navigator>
+      </AuthProvider>
     </NavigationContainer>
   );
 };
--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,10 +1,13 @@
 import { View, Text } from "react-native";
 import React from "react";
+import useAuth from "../hooks/useAuth";
 
 const LoginScreen = () => {
+  const { user } = useAuth();
+
   return (
     <View>
-      <Text>LoginScreen</Text>
+      <Text>LoginScreen {user}</Text>
     </View>
   );
 };
