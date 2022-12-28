f611ed4fa9cf1466aced5c831889c7e42e90357e
RiP3rQ <rafalpompa2000@gmail.com>
2022-12-28 19:24:46 +0100

    Screen Navigation

--- StackNavigator.js
+++ StackNavigator.js
@@ -1,14 +1,25 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { NavigationContainer } from "@react-navigation/native";
 import HomeScreen from "./screens/HomeScreen";
+import ChatScreen from "./screens/ChatScreen";
+import LoginScreen from "./screens/LoginScreen";
 
 const Stack = createNativeStackNavigator();
 
 const StackNavigator = () => {
+  const user = false;
+
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
+        {user ? (
+          <>
+            <Stack.Screen name="Home" component={HomeScreen} />
+            <Stack.Screen name="Chat" component={ChatScreen} />
+          </>
+        ) : (
+          <Stack.Screen name="Login" component={LoginScreen} />
+        )}
       </Stack.Navigator>
     </NavigationContainer>
   );
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,10 +1,17 @@
-import { View, Text } from "react-native";
+import { View, Text, Button } from "react-native";
 import React from "react";
+import { useNavigation } from "@react-navigation/native";
 
 const HomeScreen = () => {
+  const navigation = useNavigation();
+
   return (
     <View>
       <Text>HomeScreen</Text>
+      <Button
+        title="Go to chat screen"
+        onPress={() => navigation.navigate("Chat")}
+      />
     </View>
   );
 };
