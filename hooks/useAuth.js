19e6d7cc2738de3809ad8ec196478bbce64b41a6
RiP3rQ <rafalpompa2000@gmail.com>
2022-12-29 00:57:13 +0100

    implementing google auth

--- hooks/useAuth.js
+++ hooks/useAuth.js
@@ -1,6 +1,13 @@
 import React, { createContext, useContext, useEffect, useState } from "react";
 import * as Google from "expo-auth-session/providers/google";
 import * as WebBrowser from "expo-web-browser";
+import {
+  GoogleAuthProvider,
+  onAuthStateChanged,
+  signInWithCredential,
+  signOut,
+} from "@firebase/auth";
+import { auth } from "../firebase";
 
 const AuthContext = createContext({
   // initial state
@@ -16,10 +23,12 @@ const config = {
 };
 
 export const AuthProvider = ({ children }) => {
-  const [accessToken, setAccessToken] = useState(null);
+  const [error, setError] = useState(null);
   const [user, setUser] = useState(null);
+  const [loadingInitial, setLoadingInitial] = useState(true);
+  const [loading, setLoading] = useState(false);
 
-  const [request, response, promptAsync] = Google.useIdTokenAuthRequest({
+  const [request, response, promptAsync] = Google.useAuthRequest({
     androidClientId:
       "296531923790-nbfkplbb146jbu2c2es0k0moag3rggon.apps.googleusercontent.com",
     expoClientId:
@@ -29,30 +38,56 @@ export const AuthProvider = ({ children }) => {
   });
 
   useEffect(() => {
-    if (response?.type === "success") {
-      setAccessToken(response.authentication.accessToken);
-      accessToken && fetchUserInfo();
-    }
-  }, [response, accessToken]);
+    async function SignInWithGoogle() {
+      setLoading(true);
+      if (response?.type === "success") {
+        const idToken = null;
+        const { accessToken } = response.authentication;
+        const credentials = GoogleAuthProvider.credential(idToken, accessToken);
+        await signInWithCredential(auth, credentials);
+      }
 
-  async function fetchUserInfo() {
-    let response = await fetch("https://www.googleapis.com/userinfo/v2/me", {
-      headers: {
-        Authorization: `Bearer ${accessToken}`,
-      },
-    });
-    const useInfo = await response.json();
-    setUser(useInfo);
-  }
+      return Promise.reject();
+    }
+    SignInWithGoogle()
+      .catch((error) => setError(error))
+      .finally(() => setLoading(false));
+  }, [response]);
+
+  const logout = () => {
+    setLoading(true);
+
+    signOut(auth)
+      .catch((error) => setError(error))
+      .finally(() => setLoading(false));
+  };
+
+  useEffect(
+    () =>
+      onAuthStateChanged(auth, (user) => {
+        if (user) {
+          //logged in
+          setUser(user);
+        } else {
+          //NOT logged in
+          setUser(null);
+        }
+        setLoadingInitial(false);
+      }),
+    []
+  );
 
   return (
     <AuthContext.Provider
       value={{
-        user: null,
+        user,
         signIn: () => promptAsync(),
+        loading,
+        logout,
+        error,
       }}
     >
-      {children}
+      {!loadingInitial && children}
     </AuthContext.Provider>
   );
 };
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,8 +1,10 @@
 import { View, Text, Button } from "react-native";
 import { useNavigation } from "@react-navigation/native";
+import useAuth from "../hooks/useAuth";
 
 const HomeScreen = () => {
   const navigation = useNavigation();
+  const { logout } = useAuth();
 
   return (
     <View>
@@ -11,6 +13,7 @@ const HomeScreen = () => {
         title="Go to chat screen"
         onPress={() => navigation.navigate("Chat")}
       />
+      <Button title="Log out" onPress={logout} />
     </View>
   );
 };
