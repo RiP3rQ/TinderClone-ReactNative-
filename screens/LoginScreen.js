04a4ac0c02c2a42d52bcc0595eefb0c1b2328742
RiP3rQ <rafalpompa2000@gmail.com>
2022-12-29 16:10:19 +0100

    building login screen

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,13 +1,43 @@
-import { View, Text, Button } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import { useLayoutEffect } from "react";
+import {
+  View,
+  Text,
+  Button,
+  ImageBackground,
+  TouchableOpacity,
+} from "react-native";
 import useAuth from "../hooks/useAuth";
 
 const LoginScreen = () => {
   const { signIn, loading } = useAuth();
+  const navigation = useNavigation();
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShown: false,
+    });
+  }, []);
 
   return (
-    <View>
-      <Text>{loading ? "Loading..." : "LoginScreen wita"}</Text>
-      <Button title="Login" onPress={signIn} />
+    <View className="flex-1">
+      <ImageBackground
+        resizeMode="cover"
+        className="flex-1"
+        source={{
+          uri: "https://tinder.com/static/tinder.png",
+        }}
+      >
+        <TouchableOpacity
+          style={{ marginHorizontal: "25%" }}
+          className="absolute bottom-40 w-52 bg-white p-4 rounded-2xl"
+          onPress={signIn}
+        >
+          <Text className="text-center font-semibold">
+            Sign in & get swiping
+          </Text>
+        </TouchableOpacity>
+      </ImageBackground>
     </View>
   );
 };
