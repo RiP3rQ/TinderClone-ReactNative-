d0a449d9d8daab90157a1ff35df1ded0af5c902b
RiP3rQ <rafalpompa2000@gmail.com>
2022-12-30 19:08:56 +0100

    ChatRow component + getMatchedUserInfo helper function

--- StackNavigator.js
+++ StackNavigator.js
@@ -5,6 +5,7 @@ import LoginScreen from "./screens/LoginScreen";
 import useAuth, { AuthProvider } from "./hooks/useAuth";
 import ModalScreen from "./screens/ModalScreen";
 import MatchedScreen from "./screens/MatchedScreen";
+import MessageScreen from "./screens/MessageScreen";
 
 const Stack = createNativeStackNavigator();
 
@@ -22,6 +23,7 @@ const StackNavigator = () => {
           <Stack.Group>
             <Stack.Screen name="Home" component={HomeScreen} />
             <Stack.Screen name="Chat" component={ChatScreen} />
+            <Stack.Screen name="Message" component={MessageScreen} />
           </Stack.Group>
           <Stack.Group
             screenOptions={{
---
+++ screens/MessageScreen.js
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const MessagesScreen = () => {
+  return (
+    <View>
+      <Text>MessagesScreen</Text>
+    </View>
+  );
+};
+
+export default MessagesScreen;
