6d448440b6f6fe6a715f959154d4cc64177c41b9
RiP3rQ <rafalpompa2000@gmail.com>
2022-12-30 18:29:46 +0100

    header component for chatScreen

---
+++ components/Header.js
@@ -0,0 +1,26 @@
+import { View, Text, TouchableOpacity } from "react-native";
+import { Foundation, Ionicons } from "@expo/vector-icons";
+import { useNavigation } from "@react-navigation/native";
+
+const Header = ({ title, callEnabled }) => {
+  const navigation = useNavigation();
+
+  return (
+    <View className="p-2 flex-row items-center justify-between">
+      <View className="flex flex-row items-center ">
+        <TouchableOpacity onPress={() => navigation.goBack()} className="p-2">
+          <Ionicons name="chevron-back-outline" size={34} color="#FF5864" />
+        </TouchableOpacity>
+        <Text className="text-2xl font-bold pl-2">{title}</Text>
+      </View>
+
+      {callEnabled && (
+        <TouchableOpacity className="rounded-full mr-4 p-3 bg-red-200">
+          <Foundation name="telephone" size={20} color="#FF5864" />
+        </TouchableOpacity>
+      )}
+    </View>
+  );
+};
+
+export default Header;
--- screens/ChatScreen.js
+++ screens/ChatScreen.js
@@ -1,10 +1,11 @@
-import { View, Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import Header from "../components/Header";
 
 const ChatScreen = () => {
   return (
-    <View>
-      <Text>ChatScreen</Text>
-    </View>
+    <SafeAreaView>
+      <Header title="Chat" />
+    </SafeAreaView>
   );
 };
 
