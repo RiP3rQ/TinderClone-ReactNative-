49015936b75dfedb2e2238a6f01d1a91410091b1
RiP3rQ <rafalpompa2000@gmail.com>
2022-12-30 18:46:24 +0100

    ChatList component

--- components/ChatList.js
+++ components/ChatList.js
@@ -1,10 +1,42 @@
-import { View, Text } from "react-native";
-import React from "react";
+import { useEffect, useState } from "react";
+import { View, Text, FlatList } from "react-native";
+import { collection, onSnapshot, query, where } from "firebase/firestore";
+import { db } from "../firebase";
+import useAuth from "../hooks/useAuth";
+import ChatRow from "../components/ChatRow";
 
 const ChatList = () => {
-  return (
+  const [matches, setMatches] = useState([]);
+  const { user } = useAuth();
+
+  useEffect(
+    () =>
+      onSnapshot(
+        query(
+          collection(db, "matches"),
+          where("usersMatched", "array-contains", user.uid)
+        ),
+        (snapshot) =>
+          setMatches(
+            snapshot.docs.map((doc) => ({
+              id: doc.id,
+              ...doc.data(),
+            }))
+          )
+      ),
+    [user]
+  );
+
+  return matches.length > 0 ? (
+    <FlatList
+      className="h-full"
+      data={matches}
+      keyExtractor={(item) => item.id}
+      renderItem={({ item }) => <ChatRow matchDetails={item} />}
+    />
+  ) : (
     <View>
-      <Text>ChatList</Text>
+      <Text>No matches at the moment!🥶</Text>
     </View>
   );
 };
--- screens/ChatScreen.js
+++ screens/ChatScreen.js
@@ -1,10 +1,12 @@
 import { SafeAreaView } from "react-native-safe-area-context";
+import ChatList from "../components/ChatList";
 import Header from "../components/Header";
 
 const ChatScreen = () => {
   return (
     <SafeAreaView>
       <Header title="Chat" />
+      <ChatList />
     </SafeAreaView>
   );
 };
